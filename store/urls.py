8083a9d4b324e1af739fa07247d2c54ea1270cbe
ranzu123 <dhunganaranju45@gmail.com>
2020-10-27 16:58:31 +0545

    First Ecommerce site

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from store import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='update_item'),
+]
