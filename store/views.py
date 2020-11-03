2911563812c2c0efe0784086ebeff58af53392eb
prabin-g <er.prabin.gyawali@gmail.com>
2020-11-03 13:14:54 +0545

    Cart Total functionality

--- store/views.py
+++ store/views.py
@@ -8,8 +8,18 @@ from .models import *
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items()
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+        cartItems = order['get_cart_total']
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
 
@@ -18,10 +28,13 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items()
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
-    context = {'items': items, 'order': order}
+        cartItems = order['get_cart_total']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
 
@@ -30,10 +43,13 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items()
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
-    context = {'items': items, 'order': order}
+        cartItems = order['get_cart_total']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
 
